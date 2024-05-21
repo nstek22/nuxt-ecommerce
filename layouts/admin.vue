de1216f94426cf0c46fae94d3ee3624bef4b2383
nstek22 <arrido.media@gmail.com>
2024-05-21 16:00:11 +0700

    initial commit

---
+++ layouts/admin.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="c-app">
+      <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
+        <div class="c-sidebar-brand d-lg-down-none">
+          <img src="/images/ns.png" class="bg-light rounded shadow-sm p-2" width="55"> <span class="ml-2 font-weight-bold">NIZAM STORE</span>
+        </div>
+  
+        <!-- sidebar -->
+        <Sidebar />
+        <!-- end sidebar -->
+  
+      </div>
+      <div class="c-wrapper c-fixed-components">
+        
+        <!-- header -->
+        <Header />
+        <!-- end header -->
+  
+        <div class="c-body">
+  
+          <!-- content -->
+          <Nuxt />
+          <!-- end content -->
+  
+          <footer class="c-footer">
+            <div><strong>nizamstore.com</strong> &copy; 2024 - PT. Nizam Solusi Teknologi.</div>
+            <div class="ml-auto">version :&nbsp;1.0-beta</div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+    import Header from '@/components/admin/header.vue'
+    import Sidebar from '@/components/admin/sidebar.vue'
+  
+    export default {
+  
+      //middleware
+      middleware: 'isAdmin',
+  
+      //register components
+      components: {
+        Header,
+        Sidebar
+      }
+  
+    }
+  </script>
+  
+  <style>
+  </style>
---
+++ pages/categories/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container-fluid mt-custom">
+      <div class="fade-in">
+        <div class="row justify-content-center">
+            <div class="col-md-3 mb-3" v-for="category in categories" :key="category.id">
+                <div class="card border-0 rounded shadow-sm">
+                    <div class="card-body text-center">
+                        <nuxt-link :to="{name: 'categories-slug', params: {slug: category.slug}}">
+                          <img :src="category.image" width="100">
+                          <hr>
+                          {{ category.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+      //meta
+      head() {
+        return {
+          title: 'Semua Kategori - Nizamstore - Toko Jual Beli Online Murah',
+          meta: [{
+              hid: 'og:title',
+              name: 'og:title',
+              content: 'Semua Kategori - Nizamstore - Toko Jual Beli Online Murah'
+            },
+            {
+              hid: 'og:site_name',
+              name: 'og:site_name',
+              content: 'Semua Kategori - Nizamstore - Toko Jual Beli Online Murah'
+            },
+            {
+              hid: 'og:image',
+              name: 'og:image',
+              content: '/images/logo.png'
+            },
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'Semua Kategori dari Official Toko Online Penjualan Produk murah'
+            },
+          ]
+        }
+      },
+  
+      //hook "asyncData"
+      async asyncData({ store }) {
+        await store.dispatch('web/category/getCategoriesData')
+      },
+  
+      //computed
+      computed: {
+  
+        //categories
+        categories() {
+          return this.$store.state.web.category.categories
+        },
+      },
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
